ad58525d42113de390b490bb72f24a66219ecd4d
SArkarHtet <developer.soearkarhtet@gmail.com>
2020-10-01 00:51:25 +0630

    add conditional rendering

--- src/Alert.js
+++ src/Alert.js
@@ -3,13 +3,16 @@ import React, { Component } from 'react'
 export default class Alert extends Component {
   render() {
     const {error} = this.props;
+    if (error.length <= 0) {
+      return (
+        null
+      )
+    }
     return (
       <div>
-        {error.length > 0 && 
-          <div class="alert alert-danger my-3" role="alert">
-            {error}
-          </div>
-        }
+        <div class="alert alert-danger my-3" role="alert">
+          {error}
+        </div>
       </div>
     )
   }
