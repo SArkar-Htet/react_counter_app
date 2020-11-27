2344b4104dc80e804900f6833c4f14242ed2c1bf
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-11-27 19:29:12 +0000

    Deploy SArkar-Htet/react_counter_app to github.com/SArkar-Htet/react_counter_app.git:gh-pages

---
+++ precache-manifest.f09f95ff61beb4557d573624550104aa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f8453c317aafcab204da2dd98ab5fe5",
+    "url": "/react_counter_app/index.html"
+  },
+  {
+    "revision": "80d392b23008ae0e877f",
+    "url": "/react_counter_app/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "c265ab7ceb2cbfaf11ac",
+    "url": "/react_counter_app/static/css/main.3a301128.chunk.css"
+  },
+  {
+    "revision": "80d392b23008ae0e877f",
+    "url": "/react_counter_app/static/js/2.2b4e795b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_counter_app/static/js/2.2b4e795b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c265ab7ceb2cbfaf11ac",
+    "url": "/react_counter_app/static/js/main.3ac80cb9.chunk.js"
+  },
+  {
+    "revision": "d199b8d5cb1adcb95e58",
+    "url": "/react_counter_app/static/js/runtime-main.d46e97b2.js"
+  }
+]);
